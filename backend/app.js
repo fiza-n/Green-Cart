0ad5a174a02c9ae830fbf96c4bb7d950bb6ef230
fiza Noor <fnoor034726@gmail.com>
2026-08-19 09:51:01 +0500

    changes

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.set("trust proxy", 1)
 await DBConnect(process.env.MONGODB_URI)
 
 //allow multiple origins
-const allowedOrigins = ["http://localhost:5173"]
+const allowedOrigins = ["http://localhost:5173", "https://green-cart-black.vercel.app"]
 
 app.post("/online", express.raw({type: "json/application"}), stripeWebhooks)
 
